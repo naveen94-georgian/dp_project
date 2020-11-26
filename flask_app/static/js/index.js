37bc14b11aa97a719206e6bdcc732935180ddc40
naveen94-georgian <SellappanSelvaraju.Naveen@mygeorgian.ca>
2020-11-27 01:09:50 +0530

    dp_project commit 2

--- flask_app/static/js/index.js
+++ flask_app/static/js/index.js
@@ -1,13 +1,26 @@
+"use strict";
 $(function () {
     // your code goes here
     $('#tb').click(function(){
         console.log("hello");
-        $.getJSON('/get_data', {}, function(response) {
-            console.log('response: '+ response[0]);
-            for(key in response){
-                console.log(response[key]);
-            }
-          });
+        $.ajax({url: '/get_data', success: function(result){
+            console.log(result)
+            $('#my_table').children().remove()
+            $('#my_table').append(result);
+          }});
           return false;
     });
+    
+    $('a.js-toggle-trigger').click(function(event){
+        console.log(event.currentTarget.hash);
+        if(event.currentTarget.hash === '#page1'){
+            $('#page1').show();
+            $('#page2').hide();
+        }else if(event.currentTarget.hash === '#page2'){
+            $('#page2').show();
+            $('#page1').hide();
+        }
+        // $(event.currentTarget.hash).hide();
+    });
+
  });
--- flask_app/views.py
+++ flask_app/views.py
@@ -1,18 +1,22 @@
 from flask import Flask, render_template, request, json, Response, jsonify
-from flask_app.app_lib.mongo_api import MongoAPI 
+from flask_app.app_lib.mongo_api import MongoAPI
 
 app = Flask(__name__)
 
 mongo = MongoAPI(db_name='hpracdb', doc_name='house_exp')
-# lst = mongo.fetch_all()
+lst = mongo.fetch_all()
 
 @app.route("/")
 def index():
 	return render_template('index.html')
 
+# @app.route('/get_data')
+# def get_data():
+#     return Response(response=json.dumps(mongo.fetch_all()), status=200, mimetype='application/json')
+
 @app.route('/get_data')
 def get_data():
-    return Response(response=json.dumps(mongo.fetch_all()), status=200, mimetype='application/json')
+    return render_template('./components/mongo_table.html', data=lst)
 
 
 
